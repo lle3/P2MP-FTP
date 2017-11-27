187136bde2958a868fdb6bf5de5bc1d30a58cc67
Stephen Worley <sworley@cumulusnetworks.com>
2017-11-27 13:33:23 -0500

    Update scripts

--- task_1.sh
+++ task_1.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-SERVERS=("one" "two" "three" "four" "five")
+SERVERS=("18.217.114.37" "18.217.64.84" "52.15.69.135" "13.58.26.68" "18.217.111.251")
 FILE="file.txt"
 MSS=500
 
 
 # TASK 1
 
-for i in `seq 1 5`;
+for i in `seq 1 1`;
 do
     result=0
-    for j in `seq 1 5`;
+    for j in `seq 1 1`;
     do
         SECONDS=0
-        python ./p2mpclient ${SERVERS[@]:0:$i} 7735 $FILE $MSS
+        python ./p2mpclient.py ${SERVERS[@]:0:$i} 7735 $FILE $MSS
         ((result+=$SECONDS))
     done
     echo $(( result / 5 )) > task_1_nodes_$i.txt
--- task_1_server.sh
+++ task_1_server.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 PROB=0.05
-
-for i in `seq 1 5`;
+while [ 1 ]
 do
-    for j in `seq 1 5`;
-    do
-        python3 ./p2mpserver.py 7735 test.txt $PROB >> task_1.log
-    done
+    python3 ./p2mpserver.py 7735 test.txt $PROB >> task_1.log
 done
-
