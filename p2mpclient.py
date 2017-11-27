448f80a8746c7c67ec7b0a07dae5506188fdc0b4
lle3 <lle3@ncsu.edu>
2017-11-26 21:51:22 -0500

    Add almost completed sender

--- p2mpclient.py
+++ p2mpclient.py
@@ -1,4 +1,4 @@
-import sys, socketserver, struct
+import sys, socketserver, struct, threading
 
 __author__ = "Louis Le"
 __credits__ = ["Stephen Worley", "Louis Le"]
@@ -7,16 +7,25 @@ __maintainer__ = "Louis Le"
 __email__ = "lle3@ncsu.edu"
 __status__ = "Development"
 
+TIMEOUT = 30
+
+seg_counter = 0
+server_counter = 0
+server_dict
+lock = threading.Lock()
+
 class Header():
     """
         Header of the segment
     """
 
-    def __init__(self, seq_num, checksum):
+    def __init__(self, seq_num, checksum, EOT):
         # 32-bit sequence number
         self._seq_num = bytearray(seq_num)
         # 16-bit checksum of datapart, computer in the same way as the UDP checksum
         self._checksum = bytearray(checksum)
+        # 16-bit EOF flag \x00\x00 if not end of transmission, \x00\x04 if EOF
+        self._EOT = bytearray(EOT)
         # 16-bit field, indicates that this is a data packet
         # 0101010101010101
         # Using 5 because it is hexidecimal
@@ -30,92 +39,168 @@ class Header():
     def get_checksum(self):
         return self._checksum
 
+    def get_EOT(self):
+        return self._EOT
+
     # Returns string of header
     def to_bits(self):
         bits = bytearray()
         print(bits)
-        bits.append(self._seq_num[0])
-        bits.append(self._seq_num[1])
-        bits.append(self._seq_num[2])
-        bits.append(self._seq_num[3])
+        bits.extend(self._seq_num)
         print(bits)
-        bits.append(self._checksum_checksum[0])
-        bits.append(self._checksum[1])
+        bits.extend(self._checksum)
         print(bits)
-        bits.append(self._field[0])
-        bits.append(self._field[1])
+        bits.extend(self._field)
+        print(bits)
+        bits.extend(self._EOT)
         print(bits)
         return bits
 
-class MyUDPHandler(socketserver.BaseRequestHandler):
+class ThreadedUDPRequestHandler(socketserver.BaseRequestHandler):
     """
     UDP Request Handler
     """
 
     def handle(self):
+        global server_list
         data = self.request[0].strip()
-
-        seq_num, checksum, data_type = struct.unpack('>LHH', data)
-
-        socket = self.request[1]
-        print("seq: " + str(seq_num) + "\n")
-        print("check: " + str(checksum) + "\n")
-        print("type: " + str(data_type) + "\n")
-        print(data)
 
         # TODO: For testing, just inc
         seq_num = seq_num + 1
         ack = ACK((seq_num).to_bytes(4, byteorder='big'))
         socket.sendto(ack.to_bits(), self.client_address)
 
-def rdt_send(hostname, port, file):
+        server_list[hostname] = True
+        with lock:
+            server_counter += 1
+
+class ThreadedUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
+    pass
+
+def carry_around_add(a, b):
+    c = a + b
+    return (c & 0xffff) + (c >> 16)
+
+def checksum(msg):
+    global server_counter
+    s = 0
+    for i in range(0, len(msg), 2):
+        w = ord(msg[i]) + (ord(msg[i+1]) << 8)
+        s = carry_around_add(s, w)
+    return ~s & 0xffff
+
+def rdt_send(header, hostname, port, data):
     """
     Transfers data to P2MP-FTP servers
     Provides data from the file on a byte basis.
     """
+
     # SOCK_DGRAM is the socket type to use for UDP sockets
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     try:
         # As you can see, there is no connect() call; UDP has no connections.
         # Instead, data is directly sent to the recipient via sendto().
-        sock.sendto(file + "\n", (HOST, PORT))
+        sock.sendto((header.to_bits()).append(data), (hostname, port))
         received = sock.recv(1024)
+
     finally:
         sock.close()
     print("RDT SENT.\n")
 
 #checksum():
 
+def ticker():
+    """
+    """
+    global timed_out
+    life = TIMEOUT
+
+    while(life > 0):
+        time.sleep(1)
+        life -= 1
+
+def controller(servers, port, file, MSS):
+    global server_counter, seg_counter, server_dict, timed_out
+    data = file.read(MSS)
+    servers_amount = len(servers)
+    seg_counter = 0
+    first = True
+
+    while(data != ""):
+        if len(data) == MSS:
+            header = Header((seq_counter).to_bytes(4, byteorder="big"), (checksum(data)).to_bytes(2, byteorder="big"), 0)
+        else:
+            header = Header((seq_counter).to_bytes(4, byteorder="big"), (checksum(data)).to_bytes(2, byteorder="big"), 4)
+
+        server_counter = 0
+        timed_out = False
+        for hostname in servers:
+            if server_dict[hostname]:
+                continue
+            if first:
+                server_dict[hostname] = False
+                first = False
+
+            t = threading.Thread(target=rdt_send, args=[hostname, port, data])
+            t.daemon = True
+            t.start()
+
+        t = threading.Thread(target=ticker) # Starts timeout timer
+        t.daemon = True
+        t.start()
+
+        first = False
+        while server_counter != servers_amount || !timed_out:   # Loops until either the servers all acked or it times out
+            pass
+        if timed_out:   # If it times out, it will not skip below so it doesn't set up for the next segment
+            continue
+
+        first = True    # Resets dictionary to falses
+        data = file.read(MSS)   # Reads next line
+        seq_counter += 1
+        server_counter = 0
 
 if __name__ == "__main__":
     """
     Main method
     """
-
+    global server_dict
     PORT = 0
 
     try:
         HOST = "localhost"
         SERVERS, PORT, F_NAME, MSS = sys.argv[1:-3], int(sys.argv[-3]), sys.argv[-2], int(sys.argv[-1])
-        print("SERVERS: " + str(SERVERS))
-        print("PORT: " + str(PORT))
-        print("F_NAME: " + F_NAME)
-        print("MSS: " + str(MSS))
+        print("LENGTH: " + len(SERVERS) + "\n")
     except:
         print("Error in command line arguments.\
                 \nShould be in the form 'p2mpclient server-1 server-2 server-3 server-port# file-name MSS\n")
         sys.exit()
 
     try:
+        file = open(F_NAME, "r")
+    except:
+        print("File does not exit.\n")
+        sys.exit()
 
-        for server in SERVERS:
-            rdt_send(server, PORT, F_NAME)
+    try:
+        server = ThreadedUDPServer((HOST, PORT), ThreadedUDPRequestHandler)
 
-        server = socketserver.UDPServer((HOST, PORT), MyUDPHandler)
-        server.serve_forever()
+        # Start a thread with the server -- that thread will then start one
+        # more thread for each request
+        server_thread = threading.Thread(target=server.serve_forever)
+        # Exit the server thread when the main thread terminates
+        server_thread.daemon = True
+        server_thread.start()
+        print("Server loop running in thread:", server_thread.name)
+
+        # Send first parts of file to servers
+        controller(SERVERS, PORT, file, MSS)
+
+
     except(KeyboardInterrupt, SystemExit):
         print("Exiting...\n")
 
         server.shutdown()
+        server.server_close()
         sys.exit()
