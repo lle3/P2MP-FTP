2e659f8596496af63eb00586ebbb42e185102900
Stephen Worley <sworley@cumulusnetworks.com>
2017-11-27 04:13:55 -0500

    fix

--- task_1_server.sh
+++ task_1_server.sh
@@ -6,7 +6,7 @@ for i in `seq 1 5`;
 do
     for j in `seq 1 5`;
     do
-        python3 ./p2mpserver 7735 test.txt $PROB >> task_1.log
+        python3 ./p2mpserver.py 7735 test.txt $PROB >> task_1.log
     done
 done
 
