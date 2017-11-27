b60c9fb8423ae4d8ad89a49afb918257160362db
lle3 <lle3@ncsu.edu>
2017-11-27 00:26:24 -0500

    Finish Sender

--- p2mpclient.py
+++ p2mpclient.py
@@ -7,12 +7,14 @@ __maintainer__ = "Louis Le"
 __email__ = "lle3@ncsu.edu"
 __status__ = "Development"
 
-TIMEOUT = 30
+TIMEOUT = 10
 
+servers_amount = 0
 seq_counter = 0
 server_counter = 0
 server_dict = {}
 lock = threading.Lock()
+timed_out = False
 
 class Header():
     """
@@ -54,22 +56,32 @@ class Header():
 class ThreadedUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
     pass
 
-def carry_around_add(a, b):
-    c = a + b
-    return (c & 0xffff) + (c >> 16)
+def bit_add(num1, num2):
+    """
+    Adds bytes together with carryover
+    """
 
-def checksum(msg):
-    s = 0
-    for i in range(0, len(msg), 2):
-        w = msg[i] + (msg[i+1] << 8)
-        s = carry_around_add(s, w)
-    return ~s & 0xffff
+    num_sum = num1 + num2
+    return (num_sum & 0xffff) + (num_sum >> 16)
+
+
+def checksum(d):
+    """
+    Computes checksum
+    """
+
+    chk_sum = 0
+    for i in range(0, len(d), 2):
+        tmp = d[i] + (d[i+1] << 8)
+        chk_sum = bit_add(chk_sum, tmp)
+        return ~chk_sum & 0xffff
 
 def rdt_send(header, hostname, port, data):
     """
     Transfers data to P2MP-FTP servers
     Provides data from the file on a byte basis.
     """
+    global servers_amount, server_dict, server_counter
 
     # SOCK_DGRAM is the socket type to use for UDP sockets
 
@@ -87,11 +99,19 @@ def rdt_send(header, hostname, port, data):
 
         print("Received: {}".format(bytearray(received)))
 
-        print("RDT_SEND")
+        seq_num, field_1, field_2 = struct.unpack(">LHH", received)
 
+        print("seq_num: " + str(seq_num))
+        print("field_1: " + str(field_1))
+        print("field_2: " + str(field_2))
+
+        if seq_num == seq_counter:
+            with lock:
+                server_counter += 1
+                server_dict[hostname] = True
+                print("HIT")
     finally:
         sock.close()
-    print("RDT SENT.\n")
 
 #checksum():
 
@@ -103,25 +123,38 @@ def ticker():
 
     while(life > 0):
         print("LIFE: " + str(life))
+        print("SERVER_COUNTER: " + str(server_counter))
+        print("SERVER_WHAT: " + str(servers_amount))
         time.sleep(1)
         life -= 1
+
+    print("TIMEOUT")
     timed_out = True
 
 def controller(servers, port, file, MSS):
-    global server_counter, seq_counter, server_dict, timed_out
+    global server_counter, seq_counter, server_dict, timed_out, servers_amount
     data = file.read(MSS)
     servers_amount = len(servers)
     seq_counter = 0
     first = True
 
     while(data != ""):
+
+        d = data.encode()
+
         if len(data) == MSS:
-            header = Header((seq_counter).to_bytes(4, byteorder="big"), checksum(data.encode()).to_bytes(2, byteorder="big"), b'\x00\x00')
+            header = Header((seq_counter).to_bytes(4, byteorder="big"), checksum(d).to_bytes(2, byteorder="big"), b'\x00\x00')
         else:
-            header = Header((seq_counter).to_bytes(4, byteorder="big"), checksum(data.encode()).to_bytes(2, byteorder="big"), b'\x00\x04')
+            space = (MSS - len(data))
+            temp = bytearray(data.encode())
+            for i in range(space):
+                temp.extend(b"\x00")
+            data = temp.decode()
+            header = Header((seq_counter).to_bytes(4, byteorder="big"), checksum(d).to_bytes(2, byteorder="big"), b'\x00\x04')
 
         server_counter = 0
         timed_out = False
+
         for hostname in servers:
             print("HOSTNAME: " + hostname)
             if first:
@@ -139,10 +172,15 @@ def controller(servers, port, file, MSS):
         t.start()
 
         first = False
-        while server_counter != servers_amount or (timed_out == False):   # Loops until either the servers all acked or it times out
+        while not timed_out and (server_counter != servers_amount):   # Loops until either the servers all acked or it times out
             pass
+
+        print("OUT LOOP")
+        print("TIMED OUT: " + str(timed_out))
+
         if timed_out:   # If it times out, it will not skip below so it doesn't set up for the next segment
             continue
+        print("DIDN'T TIME OUT")
 
         first = True    # Resets dictionary to falses
         data = file.read(MSS)   # Reads next line
